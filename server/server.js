2bab67b6329e2b9c8af823a6c00d89d03c14d09b
kuyucucaner <tahacanerkuyucu@gmail.com>
2025-03-24 23:02:03 +0300

    Registration components

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const userRoutes = require("./routes/user-routes");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cookieParser());
+app.use(express.json());
+
+app.use("/api/v1/user" , userRoutes);
+app.get("/", (req, res) => {
+  res.send("Welcome to the Todo App API!");
+});
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
